c31fb631794321f9c25e8363f7ee409b9b9f5473
mkasiam <kawsarahmmed10001@gmail.com>
2023-11-27 21:12:02 +0600

    flat realted api created

--- index.js
+++ index.js
@@ -23,18 +23,61 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const userCollection = client.db("FlatDB").collection("users");
+    const flatCollection = client.db("FlatDB").collection("flats");
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
 
-    // User related api
+    // jwt related api
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
 
-    app.get("/users", async (req, res) => {
+    // middlewares
+    const verifyToken = (req, res, next) => {
+      // console.log('inside verify token', req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "unauthorized access" });
+      }
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "unauthorized access" });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
+
+    // flat related api
+    app.get("/flats", async (req, res) => {
+      const result = await flatCollection.find().toArray();
+      res.send(result);
+    });
+
+    // use verify admin after verifyToken
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === "admin";
+      if (!isAdmin) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      next();
+    };
+
+    // User related api
+    app.get("/users", verifyToken, verifyAdmin, async (req, res) => {
       const result = await userCollection.find().toArray();
       res.send(result);
     });
 
-    app.post("/users", async (req, res) => {
+    app.post("/users", verifyToken, async (req, res) => {
       const user = req.body;
       // insert email if user doesn't exists:
       // you can do this many ways (1. email unique, 2. upsert 3. simple checking)
