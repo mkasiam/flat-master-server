1b424de7d3dfee57d9cb8c8915a2d2477ce1cb34
mkasiam <kawsarahmmed10001@gmail.com>
2023-11-27 21:00:29 +0600

    user delete function added

--- index.js
+++ index.js
@@ -27,30 +27,32 @@ async function run() {
     // await client.connect();
     // Send a ping to confirm a successful connection
 
-
     // User related api
 
-    app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
+    app.get("/users", async (req, res) => {
       const result = await userCollection.find().toArray();
       res.send(result);
     });
-    
-    app.post('/users', async (req, res) => {
+
+    app.post("/users", async (req, res) => {
       const user = req.body;
-      // insert email if user doesnt exists: 
+      // insert email if user doesn't exists:
       // you can do this many ways (1. email unique, 2. upsert 3. simple checking)
-      const query = { email: user.email }
+      const query = { email: user.email };
       const existingUser = await userCollection.findOne(query);
       if (existingUser) {
-        return res.send({ message: 'user already exists', insertedId: null })
+        return res.send({ message: "user already exists", insertedId: null });
       }
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
 
-
-
-
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
